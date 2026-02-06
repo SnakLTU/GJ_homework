d8dae4e566159ec14188f05e512dad5a25d5e82f
Alek_Sven <aleksandras.konygin@gmail.com>
2026-02-06 14:48:30 +0200

    dinamic value for test data

--- tests/ui/data/wallpapersFlow.data.ts
+++ tests/ui/data/wallpapersFlow.data.ts
@@ -1,9 +1,14 @@
+import { faker } from '@faker-js/faker/locale/en';
+
+const seedValue: number = parseInt(process.env.TEST_SEED || '0')
+faker.seed(seedValue)
+
 interface WallpapersFlowData {
     searchKeyword: string;
     categoryOption: string;
 }
 
 export const wallpapersFlowData: WallpapersFlowData = {
-    searchKeyword: 'Nature',
+    searchKeyword: faker.animal.type(),
     categoryOption: 'Wallpapers'
 };
